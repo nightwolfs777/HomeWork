a87088c6ce8ce363268ad289d3d3c1b4601ec2d1
IldarPC <Ildarka.201995@gmail.com>
2023-04-25 21:49:17 +0500

    Finish task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -8,3 +8,12 @@ Console.WriteLine("Введите число a: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number1}");
+}
+else
+{
+    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number2}");
+}
