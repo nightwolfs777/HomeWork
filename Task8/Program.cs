0a8013d5b0710f0d14b0f1e01789428b67e0c476
IldarPC <Ildarka.201995@gmail.com>
2023-04-25 22:25:11 +0500

    Finish task8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -2,3 +2,15 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+
+}
