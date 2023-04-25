c043749a08ecdfc36aec24ec2a70e0a5335073d9
IldarPC <Ildarka.201995@gmail.com>
2023-04-25 22:51:50 +0500

    Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int second1 = number % 100;
+int second2 = second1 / 10;
+Console.WriteLine($"{number} -> {second2}");
