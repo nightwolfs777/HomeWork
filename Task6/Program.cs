46e5c261bafc081f0e1dd61c10b0654aaff716bc
IldarPC <Ildarka.201995@gmail.com>
2023-04-25 22:10:11 +0500

    Finish task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -3,3 +3,15 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
