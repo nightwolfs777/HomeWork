db93d44b02ae10da0e9d35962b2a0d98402109bd
IldarPC <Ildarka.201995@gmail.com>
2023-04-27 18:36:10 +0500

    Task19,21,23

---
+++ Task19/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (CheckingNumber(number) > 0 || CheckingNumber(number) < 0)
+{
+    if (Palindrom(number))
+    {
+        Console.WriteLine($"{number} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{number} -> нет");
+    }
+}
+
+int CheckingNumber(int num)
+{
+    if (num >= 10000 && num <= 99999) return num;
+    if (num >= -99999 && num <= -10000) return num;
+    else
+    {
+        Console.WriteLine("Введено не верное число!");
+    }
+    return num;
+}
+
+bool Palindrom(int num)
+{
+    int x1 = num / 10000; // Первая цифра числа
+    int x2 = num / 1000 % 10; // Вторая цифра числа
+    int x4 = num % 100 / 10; // Четвертая цифра числа
+    int x5 = num % 10; // Пятая цифра числа
+    if (x1 == x5 && x2 == x4)
+    {
+        return true;
+    }
+    return false;
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точки A: ");
+Console.Write("X1: ");
+int x1Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y1Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z1: ");
+int z1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("X2: ");
+int x2Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int y2Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z2: ");
+int z2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+double result = DistanceAB(x1Coordinate, y1Coordinate, z1Coordinate, x2Coordinate, y2Coordinate, z2Coordinate);
+double resultRound = Math.Round(result, 2);
+Console.WriteLine($"A ({x1Coordinate},{y1Coordinate},{z1Coordinate}) B ({x2Coordinate},{y2Coordinate},{z2Coordinate}) -> {resultRound}");
+
+double DistanceAB(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    int deltaX = x2 - x1;
+    int deltaY = y2 - y1;
+    int deltaZ = z2 - z1;
+    double distanceAB = Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+    return distanceAB;
+}
