72ba909be061ed190aa46a5a27a235176fea82ed
IldarPC <Ildarka.201995@gmail.com>
2023-04-29 15:00:44 +0500

    Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите длину массива: ");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(lenghtArray);
+PrintArray(arr);
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 99);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
