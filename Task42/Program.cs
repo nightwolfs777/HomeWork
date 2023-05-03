7d9550ee33c3d09ec164844228ccf1637d0fb6ea
IldarPC <Ildarka.201995@gmail.com>
2023-05-03 14:54:51 +0500

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет преобразововать десятичное число в двоичное.
+
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int resultBin = DecToBin(number);
+Console.WriteLine($"{number} -> {resultBin}");
+int DecToBin(int num)
+{
+    int count = 1;
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 2 * count;
+        num /= 2;
+        count *= 10;
+    }
+    return result;
+}
