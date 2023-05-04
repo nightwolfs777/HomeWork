389c2a6756d666e7fbbced50c3ac08d96abd4777
IldarPC <Ildarka.201995@gmail.com>
2023-05-04 18:20:53 +0500

    Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса четные, и замените эти элементы на их квадраты
+// 1 4 7 2    1 4 49 2
+// 5 9 2 3->  5 9  2 3
+// 8 4 2 4    64 4 4 4
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void PrintMatrixSquare(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            // if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2d);
+PrintMatrixSquare(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
