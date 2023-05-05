2ff8db437d4dd4a392a7b1441bc28bdfa05099bc
IldarPC <Ildarka.201995@gmail.com>
2023-05-05 21:00:49 +0500

    Task50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+bool PositionMatrix(int[,] matrix, int posI, int posJ)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (posI < matrix.GetLength(0) && posJ < matrix.GetLength(1)) return true;
+        }
+    }
+    return false;
+}
+
+Console.WriteLine("Введите позицию элемента в массиве по i: ");
+int positionI = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию элемента в массиве по j: ");
+int positionJ = Convert.ToInt32(Console.ReadLine());
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2d);
+bool positionMatrix = PositionMatrix(array2d, positionI, positionJ);
+Console.WriteLine(positionMatrix ? $"{positionI}, {positionJ} -> {array2d[positionI, positionJ]} " : $"{positionI}, {positionJ} -> Такого элемента в массиве нет");
